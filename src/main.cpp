9cc3ab9db479657d86d50f87fc7f63b8bd826c69
SkybowAlexandra <1973659295@qq.com>
2023-02-15 16:05:45 +0800

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <biology/animal.h>
-#include <mysql/mysql.h>
 using namespace biology;
 
 int main(int argc, char *argv[])
 {
     Cat *cat = new Cat;
-
     Speak(cat);
     delete cat;
     return 0;
